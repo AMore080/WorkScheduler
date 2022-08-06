f7645f7b9697b766879c1aa08da41ef45e280847
Alex <alexmore080@gmail.com>
2022-08-05 22:28:49 -0500

    tasks are stored to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,38 @@
+let day = document.getElementById("currentDay");
+let task = document.querySelector(".form-control")
+let save = document.querySelector(".btn")
 
-var today = moment().format("MMM Do, YYYY")
-$("currentDay").text(today);
+let taskArr = [];
+
+
+var today = moment().format("dddd, MMMM Do YYYY")
+$(day).text(today);
+
+function storeTasks(){
+    localStorage.setItem("task" , JSON.stringify(taskArr));
+}
+
+function init(){
+    let taskStor = JSON.parse(localStorage.getItem("taskArr"));
+
+    if(taskStor !== null){
+        task = taskStor;
+    }
+}
+
+save.addEventListener("click", function(event){
+    console.log("functioning");
+    event.preventDefault();
+
+    var taskText = task.value
+
+    if(taskText === ""){
+        return;
+    }
+
+    taskArr.push(taskText);
+
+    storeTasks();
+})
+
+init()
